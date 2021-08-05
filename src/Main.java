117b5f269cb35e02c7a051838135a9723ed82ca9
Tatiana Arhipova <tatianaarhipova1989@gmail.com>
2021-08-05 15:43:54 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long price = 1855;
+        int amountOf20Rubles = 1;
+        long bonus= price/20 * amountOf20Rubles ;
+        System.out.println(bonus);
+    }
+}
