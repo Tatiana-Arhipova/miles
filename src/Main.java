e96317ca42a682872283811ff33525cda9fb329a
Tatiana Arhipova <tatianaarhipova1989@gmail.com>
2021-08-13 11:52:09 +0300

    correction

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 1855;
-        int miles = service.calculate(price);
+        int miles = service.calculate(1855);
 
         System.out.println(miles);
     }
